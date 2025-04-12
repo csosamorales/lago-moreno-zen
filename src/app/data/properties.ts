d288fa4bca4097d00a9db6148f83bd4e95ebb9d9
csosamorales <camilasosamorales@gmail.com>
2025-04-12 11:00:08 -0300

    chore: add more background images

--- src/app/data/properties.ts
+++ src/app/data/properties.ts
@@ -1,22 +1,28 @@
-import { HeroSlide } from "../types";
+import { HeroSlide } from '../types';
 
 export const heroSlides: HeroSlide[] = [
-    {
-      id: 1,
-      image: '/background-1.webp',
-      title: 'Descubre Bariloche',
-      subtitle: 'Alojamientos exclusivos en el paraíso patagónico',
-    },
-    {
-      id: 2,
-      image: '/background-2.webp',
-      title: 'Experiencias Únicas',
-      subtitle: 'Vive la magia de los Andes patagónicos',
-    },
-    // {
-    //   id: 3,
-    //   image: '/background-3.webp',
-    //   title: 'Confort y Naturaleza',
-    //   subtitle: 'El balance perfecto para tus vacaciones',
-    // },
-  ];
+	{
+		id: 1,
+		image: '/background-1.webp',
+		title: 'Descubre Bariloche',
+		subtitle: 'Alojamientos exclusivos en el paraíso patagónico',
+	},
+	{
+		id: 2,
+		image: '/background-2.webp',
+		title: 'Experiencias Únicas',
+		subtitle: 'Vive la magia de los Andes patagónicos',
+	},
+	{
+		id: 3,
+		image: '/background-3.webp',
+		title: 'Confort y Naturaleza',
+		subtitle: 'El balance perfecto para tus vacaciones',
+	},
+	{
+		id: 4,
+		image: '/background-4.webp',
+		title: 'Tu Refugio en la Montaña',
+		subtitle: 'Relájate y disfruta de la belleza natural',
+	},
+];

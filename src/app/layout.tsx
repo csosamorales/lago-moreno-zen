7ddd8cec8f8ed6a0fcb5f5033d9a14b22d12f0ef
csosamorales <camilasosamorales@gmail.com>
2025-04-21 10:10:31 -0300

    feat: add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,52 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import {
+	Geist,
+	Geist_Mono,
+	Montserrat,
+	Poiret_One,
+} from 'next/font/google';
+import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 const monstserrat = Montserrat({
-  variable:'--font-montserrat',
-  subsets: ["latin"],
-})
+	variable: '--font-montserrat',
+	subsets: ['latin'],
+});
+
+const poiret = Poiret_One({
+	variable: '--font-poiret',
+	subsets: ['latin'],
+	weight: ['400'],
+});
 
 export const metadata: Metadata = {
-  title: "Lago Moreno Zen",
-  description: "Alquileres turisticos en Lago Moreno, Bariloche",
+	title: 'Lago Moreno Zen',
+	description: 'Alquileres turisticos en Lago Moreno, Bariloche',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${monstserrat.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<GoogleAnalytics gaId="G-SSXFZENWCP" />
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} ${monstserrat.variable} ${poiret.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
